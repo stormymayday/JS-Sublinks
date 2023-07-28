1319569b9a03562f551589117e0b9951a2b7fb0d
stormymayday <stormymayday7@gmail.com>
2023-07-28 11:42:35 -0700

    Implemented the Show Sidebar functionality

--- js/app.js
+++ js/app.js
@@ -6,7 +6,13 @@ const closeBtn = getElement('.close-btn');
 const sidebarWrapper = getElement('.sidebar-wrapper');
 const sidebarLinksContainer = getElement('.sidebar-links');
 const linkBtns = [...document.querySelectorAll('.link-btn')];
-const subMenu = getElement('.submenu');
-const hero = getElement('.hero');
-const navbar = getElement('.nav');
-// console.log(sidebarLinks);
+// const subMenu = getElement('.submenu');
+// const hero = getElement('.hero');
+// const navbar = getElement('.nav');
+
+// Show Sidebar Functionality
+toggleBtn.addEventListener('click', () => {
+
+    sidebarWrapper.classList.add('show');
+
+});
