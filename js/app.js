26b6885cd8a835781ca09bd6e915763e2954b70c
stormymayday <stormymayday7@gmail.com>
2023-07-28 11:16:05 -0700

    Targetting the '.close-btn'

--- js/app.js
+++ js/app.js
@@ -2,4 +2,5 @@ import sublinks from './data.js';
 import getElement from './utils/getElement.js';
 
 const toggleBtn = getElement('.toggle-btn');
-console.log(toggleBtn);
+const closeBtn = getElement('.close-btn');
+console.log(closeBtn);
