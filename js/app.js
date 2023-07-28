f14d0d4bfa18d16ff8663e4b4dfacdf18677cd40
stormymayday <stormymayday7@gmail.com>
2023-07-28 11:18:16 -0700

    Selecting the '.sidebar-links' div

--- js/app.js
+++ js/app.js
@@ -4,4 +4,5 @@ import getElement from './utils/getElement.js';
 const toggleBtn = getElement('.toggle-btn');
 const closeBtn = getElement('.close-btn');
 const sidebarWrapper = getElement('.sidebar-wrapper');
-console.log(sidebarWrapper);
+const sidebarLinks = getElement('.sidebar-links');
+console.log(sidebarLinks);
