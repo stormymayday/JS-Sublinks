a90dfbb65c90ea0113da91a10c59c81dd24d44f3
stormymayday <stormymayday7@gmail.com>
2023-07-28 11:38:31 -0700

    Selecting the '.submenu'

--- js/app.js
+++ js/app.js
@@ -6,4 +6,5 @@ const closeBtn = getElement('.close-btn');
 const sidebarWrapper = getElement('.sidebar-wrapper');
 const sidebarLinksContainer = getElement('.sidebar-links');
 const linkBtns = [...document.querySelectorAll('.link-btn')];
-console.log(sidebarLinks);
+const subMenu = getElement('.submenu');
+// console.log(sidebarLinks);
