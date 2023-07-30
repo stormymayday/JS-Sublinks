d492ca3fbc3fe11d317f82ede3e76ae6fdee00a7
stormymayday <stormymayday7@gmail.com>
2023-07-29 17:56:28 -0700

    Implemented the Sidebar

--- js/app.js
+++ js/app.js
@@ -23,3 +23,27 @@ closeBtn.addEventListener('click', () => {
     sidebarWrapper.classList.remove('show');
 
 });
+
+// console.table(sublinks);
+
+// Setting the Sidebar
+sidebarLinksContainer.innerHTML = sublinks.map((item) => {
+
+    // console.log(item);
+
+    const { page, links } = item;
+
+    return `<article>
+    <h4>${page}</h4>
+    <div class="sidebar-sublinks">
+    ${links.map((link) => {
+
+        // console.log(link);
+
+        return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`;
+
+    }).join('')}
+    </div>
+    </article>`;
+
+}).join('');
